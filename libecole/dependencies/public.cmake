37efe76d5aa36920479c02a6e5984e8e9b44e078
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-12-19 19:33:18 +0100

    Update built-in dependencies

--- libecole/dependencies/public.cmake
+++ libecole/dependencies/public.cmake
@@ -1,28 +1,28 @@
 find_or_download_package(
 	NAME xtl
-	URL https://github.com/xtensor-stack/xtl/archive/0.7.2.tar.gz
-	URL_HASH SHA256=95c221bdc6eaba592878090916383e5b9390a076828552256693d5d97f78357c
+	URL https://github.com/xtensor-stack/xtl/archive/0.8.1.tar.gz
+	URL_HASH SHA256=e69a696068ccffd2b435539d583665981b6c6abed596a72832bffbe3e13e1f49
 	CONFIGURE_ARGS -D BUILD_TESTS=OFF
 )
 
 find_or_download_package(
 	NAME xsimd
-	URL https://github.com/xtensor-stack/xsimd/archive/7.4.9.tar.gz
-	URL_HASH SHA256=f6601ffb002864ec0dc6013efd9f7a72d756418857c2d893be0644a2f041874e
+	URL https://github.com/xtensor-stack/xsimd/archive/14.0.0.tar.gz
+	URL_HASH SHA256=17de0236954955c10c09d6938d4c5f3a3b92d31be5dadd1d5d09fc1b15490dce
 	CONFIGURE_ARGS -D BUILD_TESTS=OFF
 )
 
 find_or_download_package(
 	NAME xtensor
-	URL https://github.com/xtensor-stack/xtensor/archive/0.23.1.tar.gz
-	URL_HASH SHA256=b9bceea49db240ab64eede3776d0103bb0503d9d1f3ce5b90b0f06a0d8ac5f08
+	URL https://github.com/xtensor-stack/xtensor/archive/0.27.1.tar.gz
+	URL_HASH SHA256=117c192ae3b7c37c0156dedaa88038e0599a6b264666c3c6c2553154b500fe23
 	CONFIGURE_ARGS -D BUILD_TESTS=OFF
 )
 
 find_or_download_package(
 	NAME span-lite
-	URL https://github.com/martinmoene/span-lite/archive/v0.9.0.tar.gz
-	URL_HASH SHA256=cdb5f86e5f5e679d63700a56de734c44fe22a574a17347d09dbaaef80619af91
+	URL https://github.com/martinmoene/span-lite/archive/v0.11.0.tar.gz
+	URL_HASH SHA256=ef4e028e18ff21044da4b4641ca1bc8a2e2d656e2028322876c0e1b9b6904f9d
 	CONFIGURE_ARGS
 		-D SPAN_LITE_OPT_BUILD_TESTS=OFF
 		-D SPAN_LITE_OPT_BUILD_EXAMPLES=OFF

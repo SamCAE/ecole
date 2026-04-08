c7707e2a492c4be9dc6847162e4579b3c6c35f45
SamCAE <samer.dabbas10@gmail.com>
2026-04-08 19:02:37 +0300

    Support for SCIP 10

--- cmake/DependenciesResolver.cmake
+++ cmake/DependenciesResolver.cmake
@@ -9,6 +9,9 @@
 if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
 	cmake_policy(SET CMP0135 NEW)
 endif()
+if(POLICY CMP0169)
+	cmake_policy(SET CMP0169 OLD)
+endif()
 
 include(FetchContent)
 
@@ -43,9 +46,50 @@ function(build_package)
 	set(multiValueArgs CONFIGURE_ARGS)
 	cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
+	set(dep_cmake_prefix_path "${ARG_INSTALL_DIR}")
+	if(CMAKE_PREFIX_PATH)
+		list(APPEND dep_cmake_prefix_path ${CMAKE_PREFIX_PATH})
+	endif()
+	list(JOIN dep_cmake_prefix_path ";" dep_cmake_prefix_path)
+
+	set(dep_forwarded_args)
+	if(DEFINED Python_EXECUTABLE)
+		list(APPEND dep_forwarded_args -D "Python_EXECUTABLE=${Python_EXECUTABLE}")
+	endif()
+	if(DEFINED PYTHON_EXECUTABLE)
+		list(APPEND dep_forwarded_args -D "PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}")
+	endif()
+	if(DEFINED pybind11_DIR)
+		list(APPEND dep_forwarded_args -D "pybind11_DIR=${pybind11_DIR}")
+	endif()
+	if(DEFINED Python_NumPy_INCLUDE_DIRS)
+		list(JOIN Python_NumPy_INCLUDE_DIRS ";" dep_numpy_include_dirs)
+	elseif(DEFINED Python_EXECUTABLE)
+		execute_process(
+			COMMAND "${Python_EXECUTABLE}" -c "import numpy; print(numpy.get_include())"
+			RESULT_VARIABLE dep_numpy_error
+			OUTPUT_VARIABLE dep_numpy_include_dirs
+			ERROR_QUIET
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		if(dep_numpy_error)
+			unset(dep_numpy_include_dirs)
+		endif()
+	endif()
+	if(DEFINED dep_numpy_include_dirs AND NOT dep_numpy_include_dirs STREQUAL "")
+		list(APPEND dep_forwarded_args -D "NUMPY_INCLUDE_DIRS=${dep_numpy_include_dirs}")
+	endif()
+
 	message(DEBUG "${CMAKE_COMMAND}" -S "${ARG_SOURCE_DIR}" -B "${ARG_BUILD_DIR}" ${ARG_CONFIGURE_ARGS})
 	execute_process_handle_output(
-		COMMAND "${CMAKE_COMMAND}" -S "${ARG_SOURCE_DIR}" -B "${ARG_BUILD_DIR}" -G "${CMAKE_GENERATOR}" ${ARG_CONFIGURE_ARGS}
+		COMMAND
+			"${CMAKE_COMMAND}"
+			-S "${ARG_SOURCE_DIR}"
+			-B "${ARG_BUILD_DIR}"
+			-G "${CMAKE_GENERATOR}"
+			-D "CMAKE_PREFIX_PATH=${dep_cmake_prefix_path}"
+			${dep_forwarded_args}
+			${ARG_CONFIGURE_ARGS}
 	)
 	execute_process_handle_output(COMMAND "${CMAKE_COMMAND}" --build "${ARG_BUILD_DIR}" --parallel)
 	execute_process_handle_output(COMMAND "${CMAKE_COMMAND}" --install "${ARG_BUILD_DIR}" --prefix "${ARG_INSTALL_DIR}")
@@ -78,12 +122,12 @@ macro(find_or_download_package)
 			FetchContent_GetProperties(${ARG_NAME})
 			if(NOT ${ARG_NAME}_POPULATED)
 				message(STATUS "Downloading ${ARG_NAME}")
-				FetchContent_MakeAvailable(${ARG_NAME})
+				FetchContent_Populate(${ARG_NAME})
 				message(STATUS "Building ${ARG_NAME}")
 				# FetchContent_Populate uses lower case name of FetchContent_Declare for directories
 				string(TOLOWER "${ARG_NAME}" ARG_NAME_LOWER)
 				build_package(
-					CONFIGURE_ARGS ${ARG_CONFIGURE_ARGS} -D "CMAKE_PREFIX_PATH=${FETCHCONTENT_INSTALL_DIR}"
+					CONFIGURE_ARGS ${ARG_CONFIGURE_ARGS}
 					SOURCE_DIR "${${ARG_NAME_LOWER}_SOURCE_DIR}"
 					BUILD_DIR "${${ARG_NAME_LOWER}_BINARY_DIR}"
 					INSTALL_DIR "${FETCHCONTENT_INSTALL_DIR}"
--- python/extension-helper/dependencies/public.cmake
+++ python/extension-helper/dependencies/public.cmake
@@ -1,21 +1,55 @@
 find_package(Python COMPONENTS Interpreter Development NumPy REQUIRED)
 
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+	cmake_policy(SET CMP0135 NEW)
+endif()
+if(POLICY CMP0169)
+	cmake_policy(SET CMP0169 OLD)
+endif()
+
 find_or_download_package(
 	NAME pybind11
-	URL https://github.com/pybind/pybind11/archive/v2.13.6.tar.gz
-	URL_HASH SHA256=e08cb87f4773da97fa7b5f035de8763abc656d87d5773e62f6da0587d1f0ec20
+	URL https://github.com/pybind/pybind11/archive/v3.0.3.tar.gz
+	URL_HASH SHA256=787459e1e186ee82001759508fefa408373eae8a076ffe0078b126c6f8f0ec5e
 	CONFIGURE_ARGS
 		-D PYBIND11_TEST=OFF
 		-D "Python_EXECUTABLE=${Python_EXECUTABLE}"
 		-D "PYTHON_EXECUTABLE=${Python_EXECUTABLE}"
 )
 
-find_or_download_package(
-	NAME xtensor-python
-	URL https://github.com/xtensor-stack/xtensor-python/archive/0.29.0.tar.gz
-	URL_HASH SHA256=2915b220bd11b70fdd9fbb2db5f313e751189fec083e406228c8e5a31dfaa4a2
-	CONFIGURE_ARGS
-		-D BUILD_TESTS=OFF
-		-D "Python_EXECUTABLE=${Python_EXECUTABLE}"
-		-D "PYTHON_EXECUTABLE=${Python_EXECUTABLE}"
-)
+find_package(xtensor-python QUIET)
+
+if(NOT TARGET xtensor-python)
+	execute_process(
+		COMMAND "${Python_EXECUTABLE}" -c "import numpy; print(numpy.get_include())"
+		RESULT_VARIABLE xtensor_python_numpy_error
+		OUTPUT_VARIABLE xtensor_python_numpy_include
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	if(xtensor_python_numpy_error)
+		message(FATAL_ERROR "Failed to locate NumPy include path with ${Python_EXECUTABLE}.")
+	endif()
+
+	FetchContent_Declare(
+		xtensor-python
+		URL https://github.com/xtensor-stack/xtensor-python/archive/0.29.0.tar.gz
+		URL_HASH SHA256=2915b220bd11b70fdd9fbb2db5f313e751189fec083e406228c8e5a31dfaa4a2
+	)
+	FetchContent_GetProperties(xtensor-python)
+	if(NOT xtensor-python_POPULATED)
+		message(STATUS "Downloading xtensor-python")
+		FetchContent_Populate(xtensor-python)
+		message(STATUS "Building xtensor-python")
+		build_package(
+			CONFIGURE_ARGS
+				-D BUILD_TESTS=OFF
+				-D "NUMPY_INCLUDE_DIRS=${xtensor_python_numpy_include}"
+				-D "Python_EXECUTABLE=${Python_EXECUTABLE}"
+				-D "PYTHON_EXECUTABLE=${Python_EXECUTABLE}"
+			SOURCE_DIR "${xtensor-python_SOURCE_DIR}"
+			BUILD_DIR "${xtensor-python_BINARY_DIR}"
+			INSTALL_DIR "${FETCHCONTENT_INSTALL_DIR}"
+		)
+		find_package(xtensor-python PATHS "${FETCHCONTENT_INSTALL_DIR}" NO_DEFAULT_PATH QUIET)
+	endif()
+endif()
